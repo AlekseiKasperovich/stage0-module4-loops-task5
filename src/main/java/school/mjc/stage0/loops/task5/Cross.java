0ff8cb0e018fb1ff39655d92137bfce3262bb01e
AlekseiKasperovich <AleksKasperovich@yandex.by>
2022-12-13 14:45:02 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,7 +1,17 @@
 package school.mjc.stage0.loops.task5;
 
 public class Cross {
-    public void printCross(int sideLength) {
 
+    public void printCross(int sideLength) {
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i == j || j == sideLength - 1 - i) {
+                    System.out.print(8);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
